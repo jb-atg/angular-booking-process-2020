b90ffecf50de260fc00f238f7e098244a45b348f
Justin Beattie <justinbeattie@justin.local>
2020-01-04 14:25:17 +1100

    grid

--- src/app/shared/grid/cell/cell.component.ts
+++ src/app/shared/grid/cell/cell.component.ts
@@ -1,15 +1,91 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, Input, OnChanges, Renderer2, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'cell',
   templateUrl: './cell.component.html',
   styleUrls: ['./cell.component.scss']
 })
-export class CellComponent implements OnInit {
+export class CellComponent implements OnChanges, OnDestroy {
+  id: string;
+  styleTag: Node;
 
-  constructor() { }
+  @Input('position') _position: any;
+  position: object = {};
 
-  ngOnInit() {
+  @Input('size') _size: any;
+  size: object = {};
+
+  constructor(private renderer: Renderer2, private element: ElementRef) { }
+
+  ngOnChanges(): void {
+    if (this._position || this._size) {
+      this.position = this.parseInputValue(this._position);
+      this.size = this.parseInputValue(this._size);
+      this.createStylesheet();
+    } else if (this.styleTag) {
+      this.renderer.removeChild(window.document.head, this.styleTag);
+    }
   }
 
+  parseInputValue(inputValue) {
+    if (inputValue) {
+      if (inputValue === Object(inputValue)  /* e.g. [position]="{xs:'x3 y2',sm:'x3 y2',md:'x3 y2',lg:'x3 y2',xl:'x3 y2'}"*/) {
+        return inputValue;
+      } else if (!inputValue.includes(":")/* e.g. position="x3 y2" */) {
+        let staticValue: string = inputValue;
+        return { xs: staticValue, sm: staticValue, md: staticValue, lg: staticValue, xl: staticValue };
+      } else if (typeof inputValue == 'string' && inputValue.includes(":") && inputValue.includes(",") /* e.g. position="xs:x3 y2,sm:x3 y2,md:x3 y2,lg:x3 y2,xl:x3 y2"*/) {
+        inputValue = inputValue.replace(/:/g, ":'");
+        inputValue = inputValue.replace(/,/g, "',");
+        inputValue = inputValue.charAt(inputValue.length - 1) != "," ? inputValue.concat("'") : null;
+        return eval("(" + '{' + inputValue + '}' + ")");
+      } else {
+        console.log(inputValue + 'is invalid');
+      }
+    } else {
+      return { xs: 0, sm: 0, md: 0, lg: 0, xl: 0 };//
+    }
+  }
+
+  createStylesheet() {
+    this.id = 'cell-' + Math.random().toString(36).substring(2, 8);
+
+    this.styleTag ? this.renderer.removeChild(window.document.head, this.styleTag) : null;
+    this.styleTag = this.renderer.createElement('style');
+    const breakpoints = ['xs', 'sm', 'md', 'lg', 'xl'];
+    let css: string = '';
+
+    breakpoints.forEach((breakpoint) => {
+      this.position[breakpoint] = this.position[breakpoint].replace(/x|X/g, "x:").replace(/y|y/g, "y:").replace(/ /g, ",");
+      this.position[breakpoint] = eval("(" + '{' + this.position[breakpoint] + '}' + ")");
+
+      this.size[breakpoint] = 'x:' + this.size[breakpoint].replace(/x|X/g, ",y:");
+      this.size[breakpoint] = eval("(" + '{' + this.size[breakpoint] + '}' + ")");
+
+      css = css +
+        ` .${breakpoint} #${this.id}  {
+        
+        grid-column-start: ${this.position[breakpoint].x};
+        -ms-grid-column: ${this.position[breakpoint].x};
+
+        grid-column-end: span ${this.size[breakpoint].x};
+        -ms-grid-column-span: ${this.size[breakpoint].x};
+
+        grid-row-start: ${this.position[breakpoint].y};
+        -ms-grid-row: ${this.position[breakpoint].y};
+
+        grid-row-end: span ${this.size[breakpoint].y};
+        -ms-grid-row-span: ${this.size[breakpoint].y};
+
+          }`;
+    });
+
+    this.renderer.appendChild(this.styleTag, this.renderer.createText(css));
+    this.renderer.appendChild(window.document.head, this.styleTag);
+    this.renderer.setAttribute(this.element.nativeElement, 'id', this.id);
+  }
+
+  ngOnDestroy(): void {
+    this.styleTag ? this.renderer.removeChild(window.document.head, this.styleTag) : null;
+  }
 }
--- src/app/shared/grid/grid.component.ts
+++ src/app/shared/grid/grid.component.ts
@@ -1,190 +1,83 @@
-import { Component, OnInit, ViewChild, ElementRef, AfterViewInit, Input, OnChanges, Renderer2, OnDestroy } from '@angular/core';
-import { Breakpoints } from '@angular/cdk/layout';
+import { Component, ElementRef, Input, OnChanges, Renderer2, OnDestroy } from '@angular/core';
 
 @Component({
   selector: 'grid',
   templateUrl: './grid.component.html',
   styleUrls: ['./grid.component.scss']
 })
-export class GridComponent implements OnInit, AfterViewInit, OnChanges, OnDestroy {
-
-  breakpoints = { static: [], xs: [], sm: [], md: [], lg: [], xl: [] };
-  cssProperties = {
-    cols: [
-      { name: 'grid-template-columns', value: '', unit: 'fr' },
-      { name: '-ms-grid-columns', value: '', unit: 'fr' },],
-    rows: [
-      { name: 'grid-template-rows', value: '', unit: 'fr' },
-      { name: '-ms-grid-rows', value: '', unit: 'fr' }],
-    gap: [
-      { name: 'grid-gap', value: '', unit: 'rem' },
-      { name: 'padding', value: '', unit: 'rem' },
-      { name: 'width', value: '', unit: 'rem' }
-    ],
-  }
-
-  @Input('cols') _cols: any;
-  cols: object;
-
-  @Input('rows') _rows: any;
-  rows: object;
-
-  @Input('gap') _gap: any;
-  gap: object;
-
+export class GridComponent implements OnChanges, OnDestroy {
   id: string;
   styleTag: Node;
+
+  @Input('cols') _cols: any;
+  cols: object = {};
+
+  @Input('rows') _rows: any;
+  rows: object = {};
+
+  @Input('gap') _gap: any;
+  gap: object = {};
 
 
   constructor(private renderer: Renderer2, private element: ElementRef) { }
 
-  ngOnInit() {
-
-  }
-
-  ngOnChanges() {
-    if (this.cols || this.rows) {
-      this.getInputValue(this.cols, 'cols');
-      this.getInputValue(this.rows, 'rows');
-      this.getInputValue(this.gap, 'gap');
-  /*     this.createStylesheet(); */
+  ngOnChanges(): void {
+    if (this._cols || this._rows || this._gap) {
+      this.cols = this.parseInputValue(this._cols);
+      this.rows = this.parseInputValue(this._rows);
+      this.gap = this.parseInputValue(this._gap);
+      this.createStylesheet();
+    } else if (this.styleTag) {
+      this.renderer.removeChild(window.document.head, this.styleTag);
     }
   }
 
-  ngAfterViewInit(): void {
-    if (this.cols || this.rows) {
-      this.id = 'grid-' + Math.random().toString(36).substring(2, 8);
-      this.renderer.setAttribute(this.element.nativeElement, 'id', this.id);
+  parseInputValue(inputValue) {
+    if (inputValue) {
+      if (inputValue === Object(inputValue)  /* e.g. [cols]="{xs:3,sm:4,md:5,lg:6,xl:7}"*/) {
+        return inputValue;
+      } else if (!inputValue.includes(":")/* e.g. cols="3" */) {
+        let staticValue = typeof inputValue == 'string' ? Number(inputValue) : inputValue;
+        return { xs: staticValue, sm: staticValue, md: staticValue, lg: staticValue, xl: staticValue };
+      } else if (typeof inputValue == 'string' && inputValue.includes(":") && inputValue.includes(",") /* e.g. cols="xs:3,sm:4,md:5,lg:6,xl:7"*/) {
+        return eval("(" + '{' + inputValue + '}' + ")");
+      } else {
+        console.log(inputValue + 'is invalid');
+      }
+    } else {
+      return { xs: 0, sm: 0, md: 0, lg: 0, xl: 0 };
     }
+  }
+
+  createStylesheet() {
+   this.id = 'grid-' + Math.random().toString(36).substring(2, 8);
+
+    this.styleTag ? this.renderer.removeChild(window.document.head, this.styleTag) : null;
+    this.styleTag = this.renderer.createElement('style');
+    const breakpoints = ['xs', 'sm', 'md', 'lg', 'xl'];
+    let css: string = '';
+
+    breakpoints.forEach((breakpoint) => {
+      css = css +
+        ` .${breakpoint} #${this.id}  {
+        grid-template-columns: ${' 1fr '.repeat(this.cols[breakpoint])};
+        -ms-grid-columns: ${' 1fr '.repeat(this.cols[breakpoint])};
+        grid-template-rows: ${' 1fr '.repeat(this.rows[breakpoint])};
+        -ms-grid:-rows: ${' 1fr '.repeat(this.rows[breakpoint])};
+        grid-gap: ${this.gap[breakpoint] + 'rem'};
+        padding: ${this.gap[breakpoint] + 'rem'};
+        width: calc(100% - ${(this.gap[breakpoint] * 2) + 'rem'});
+        height: calc(100% - ${(this.gap[breakpoint] * 2) + 'rem'});
+          }`;
+    });
+
+    this.renderer.appendChild(this.styleTag, this.renderer.createText(css));
+    this.renderer.appendChild(window.document.head, this.styleTag);
+    this.renderer.setAttribute(this.element.nativeElement, 'id', this.id);
   }
 
   ngOnDestroy(): void {
     this.styleTag ? this.renderer.removeChild(window.document.head, this.styleTag) : null;
   }
-
-  getInputValue(value, propertyName) {
-    if (value.length == 1 || typeof value == 'number' || value.includes(".")) {
-      this[propertyName].static = typeof value == 'string' ? Number(value) : value;
-    } else if (value === Object(value)) {
-      this[propertyName] = value;
-    } else if (typeof value == 'string') {
-      this[propertyName] = eval("(" + '{' + value + '}' + ")");
-    }
-  }
-
-  setStyles(propertyName) {
-    Object.keys(this.breakpoints).forEach((bp, i) => {
-      if (this[propertyName][bp]) {
-        this.breakpoints[bp].push(this.cssProperties[propertyName]);
-        this.setCssValues(propertyName, bp);
-      }
-    });
-  }
-
-
-  setCssValues(propertyName, bp) {
-    this.breakpoints[bp].forEach((property, i) => {
-      property.value = this[propertyName][bp];
-    });
-  }
-
-
-
-
-
-
-
-
-  /*   abc = [
-      { name: '', value: '', repeated: true }
-    ] */
-  /* 
-    setBreakpoints(staticValue: number, responsiveValue: object, cssProperties: Array<any>) {
-      let value: string;
-      let id: string = ' #' + this.id;
-   
-      if (staticValue) {
-        value = id + '{' +
-          cssProperties.forEach((cssProperty, i) => {
-            if (cssProperty.value) {
-              return cssProperty.name + ':' + cssProperty.value + ';'
-            }
-          });
-        + '}';
-   
-      } else if (responsiveValue) {
-        value = '' + Object.keys(responsiveValue).forEach((bp, i) => {
-          let responsiveClassName: string = ' .' + bp;
-          return responsiveClassName + id + '{' +
-            cssProperties.forEach((cssProperty, i) => {
-              if (cssProperty.value) {
-                return cssProperty.name + ':' + cssProperty.value + ';'
-              }
-            });
-          + '}';
-        });
-   
-        return value
-   
-      }
-    }
-   */
-
-  createStylesheet1() {
-    // Remove previous style sheet
-    if (this.styleTag) { this.renderer.removeChild(window.document.head, this.styleTag); }
-
-    let tag = this.renderer.createElement('style');
-    let stylesheet = '';
-
-    let textNode = this.renderer.createText(stylesheet);
-    this.renderer.appendChild(tag, textNode);
-    this.styleTag = tag;
-    this.renderer.appendChild(window.document.head, this.styleTag);
-
-  }
-
-/*   createStylesheet() {
-
-    this.styleTag ? this.renderer.removeChild(window.document.head, this.styleTag) : null;
-    this.id ? this.renderer.removeAttribute(this.element.nativeElement, 'id', this.id) : null;
-
-    this.id = 'grid-' + Math.random().toString(36).substring(2, 8);
-    this.renderer.setAttribute(this.element.nativeElement, 'id', this.id);
-
-    let tag = this.renderer.createElement('style');
-
-    let stylesheet = '';
-    if (this.staticCols || this.staticRows || this.staticGap) {
-      let columns = this.staticCols ? ' 1fr '.repeat(this.staticCols) : null;
-      let rows = this.staticRows ? ' 1fr '.repeat(this.staticRows) : null;
-      let gap = this.staticGap ? this.staticGap + 'rem' : null;
-      stylesheet =
-        ` #${this.id} {
-        ${columns ? 'grid-template-columns:' + columns + ';' + '-ms-grid-colums' + columns + ';' : ''}
-        ${rows ? 'grid-template-rows:' + rows + ';' + '-ms-grid-rows' + rows + ';' : ''}
-        ${gap ? 'grid-gap:' + gap + ';' + 'padding:' + gap + ';' + 'width: calc(100% - ' + gap + ' - ' + gap + ');' : ''}
-          }`;
-    }
-    if (this.responsiveCols || this.responsiveRows || this.responsiveGap) {
-      ['xs', 'sm', 'md', 'lg', 'xl'].forEach(bp => {
-        let columns = this.responsiveCols ? ' 1fr '.repeat(this.responsiveCols[bp]) : null;
-        let rows = this.responsiveRows ? ' 1fr '.repeat(this.responsiveRows[bp]) : null;
-        let gap = this.responsiveGap ? this.responsiveGap[this.responsiveGap[bp]] + 'rem' : null;
-
-        stylesheet = stylesheet +
-          ` .${bp} #${this.id}  {
-            ${columns ? 'grid-template-columns:' + columns + ';' + '-ms-grid-colums' + columns + ';' : ''}
-            ${rows ? 'grid-template-rows:' + rows + ';' + '-ms-grid-rows' + rows + ';' : ''}
-            ${gap ? 'grid-gap:' + gap + ';' + 'padding:' + gap + ';' + 'width: calc(100% - ' + gap + ' - ' + gap + ');' : ''}
-              }`;
-      });
-    }
-    let textNode = this.renderer.createText(stylesheet);
-    this.renderer.appendChild(tag, textNode);
-    this.styleTag = tag;
-    this.renderer.appendChild(window.document.head, this.styleTag);
-
-  } */
 
 }
